af1f1026eeab7ca3b9f04a7e46b60a28fdc91c84
Jefferson Soares <jefferson1104junior@gmail.com>
2023-06-07 17:03:50 -0400

    mobile: create main screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,60 @@
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { styled } from 'nativewind'
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import Logo from './src/assets/spacetime-logo.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    BaiJamjuree_700Bold,
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-2xl font-bold text-gray-50">Jefferson Soares</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center px-8 py-10 bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe{' '}
+            (se quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity activeOpacity={0.7} className="rounded-full bg-green-500 px-5 py-2">
+          <Text className="font-alt text-sm uppercase leading-none text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 por Soares Dev LTDA.
+      </Text>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
