82fd5862a3e0192175b86de186a31b01e4a7e1a1
Jefferson Soares <jefferson1104junior@gmail.com>
2023-06-19 14:45:40 -0400

    frontend: create components

---
+++ frontend/src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+// COPYRIGHT COMPONENT
+export function Copyright() {
+  return(
+    <div className="text sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        className="underline hover:text-gray-100 transition-colors"
+        href="https://soaresdev.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Soares Dev
+      </a>
+    </div>
+  )
+}
