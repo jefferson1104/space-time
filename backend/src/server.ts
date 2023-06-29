d7dcc877534c4c3a2bab8d902708a9a079ba73c3
Jefferson Soares <jefferson1104junior@gmail.com>
2023-06-29 10:46:29 -0400

    frontend: logout route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,11 +11,11 @@ const app = fastify()
 
 // if you want change the API to open for all, put the value 'origin: true'
 app.register(cors, {
-  origin: ['http://localhost:3000', 'https://yourdomain.com']
+  origin: ['http://localhost:3000', 'https://yourdomain.com'],
 })
 
 app.register(jwt, {
-  secret: 'spacetime_soaresdev'
+  secret: 'spacetime_soaresdev',
 })
 
 app.register(authRoutes)
@@ -24,7 +24,7 @@ app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,
-    // host: '0.0.0.0',
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
--- frontend/src/app/api/auth/callback/route.ts
+++ frontend/src/app/api/auth/callback/route.ts
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
-import { api } from "@/lib/api";
+import { api } from '@/lib/api'
 
 export async function GET(request: NextRequest) {
-    const { searchParams } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
 
-    const code = searchParams.get('code')
+  const code = searchParams.get('code')
 
-    const redirectTo = request.cookies.get('redirectTo')?.value
+  const redirectTo = request.cookies.get('redirectTo')?.value
 
-    const registerResponse = await api.post('/register', {
-        code,
-    })
+  const registerResponse = await api.post('/register', {
+    code,
+  })
 
-    const { token } = registerResponse.data
+  const { token } = registerResponse.data
 
-    const redirectUrl = new URL(redirectTo ?? '/', request.url)
+  const redirectUrl = new URL(redirectTo ?? '/', request.url)
 
-    const cookieExpiresIn = 60 * 60 * 24 * 30 // 30 days
+  const cookieExpiresIn = 60 * 60 * 24 * 30 // 30 days
 
-    return NextResponse.redirect(redirectUrl, {
-        headers: {
-          'Set-Cookie': `_spacetime_token=${token}; Path=/; max-age=${cookieExpiresIn};`,
-        },
-    })
+  return NextResponse.redirect(redirectUrl, {
+    headers: {
+      'Set-Cookie': `_spacetime_token=${token}; Path=/; max-age=${cookieExpiresIn};`,
+    },
+  })
 }
