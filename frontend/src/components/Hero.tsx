0a0ee2b97690ce77b9f4c937263fece879b86ee7
Jefferson Soares <jefferson1104junior@gmail.com>
2023-07-12 21:32:09 -0400

    frontend: new memory

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -7,13 +7,16 @@ import logo from '../assets/space-time.svg'
 // HERO COMPONENT
 export function Hero() {
   return (
-      <div className="space-y-5">
-        <Image src={logo} alt="spacetime logo" />
+    <div className="space-y-5">
+      <Image src={logo} alt="spacetime logo" />
       <div className="max-w-[420px] space-y-1">
-      <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
-      <p className="text-lg leading-relaxed font-sans">
-        Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
-      </p>
+        <h1 className="text-5xl font-bold leading-tight text-gray-50">
+          Sua cápsula do tempo
+        </h1>
+        <p className="font-sans text-lg leading-relaxed">
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+          com o mundo!
+        </p>
       </div>
       <Link
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
